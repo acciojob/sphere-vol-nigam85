cca57be245470ecb1da168058d6c82c7fc51f678
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-08 12:44:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
-// function volume_sphere(event) {
-//     //Write your code here
-// 	// var radius = parseFloat(document.getElementById("radius").value);
-
-//  //  // Calculate the volume of the sphere using the formula V = (4/3) * π * r^3
-//  //  var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-
-//  //  // Display the volume in the volume field
-//  //  document.getElementById("volume").value = volume.toFixed(2);
-// 	// CalculateButton.addEventListener('click',calculateVolume)
-
-// 	event.preventDefault();
-// 	let item = parseFloat(document.getElementById('radius').value);
-// 	let vol = document.getElementById('volume');
-// 	vol.inputMode.value = none;
-// 	let result = 4/3 * Math.PI * item * item * item;
-// 	console.log(result);
-
-// 	vol.value = result.toFixed(2);
-	
-// } 
-
-// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
-
 function volume_sphere(event) {
-    //Write your code here
-	event.preventDefault();
-	let item = parseFloat(document.getElementById('radius').value);
-  let vol = document.getElementById('volume');
-  vol.inputMode.value = none;
-	let result =  4/3 * Math.PI*item *item*item;
-  console.log(result);
+//Write your code here
+//  var radius = parseFloat(document.getElementById("radius").value);
 
-	vol.value = result.toFixed(2);
-	} 
+//  Calculate the volume of the sphere using the formula V = (4/3) * π * r^3
+// var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+//  Display the volume in the volume field
+//   document.getElementById("volume").value = volume.toFixed(2);
+//  CalculateButton.addEventListener('click',calculateVolume)
+  const radiusInput = document.getElementById("radius");
+            const radius = parseFloat(radiusInput.value);
+
+            // calculate the volume of the sphere
+            const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+            // display the volume in the volume field
+            const volumeField = document.getElementById("volume");
+            volumeField.value = volume.toFixed(4); // round to 2 decimal places
+        }
+
+
+ }
+ window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
